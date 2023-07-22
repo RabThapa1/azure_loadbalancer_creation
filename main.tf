a692269b7d201f3544650565f98767da24cdf5fd
RabThapa1 <138276412+RabThapa1@users.noreply.github.com>
2023-07-23 11:15:18 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,42 @@
-resource "azurerm_resource_group" "rg"{
-    name = "rg-np-loadbalancer"
-    location = "australiaeast"
+                                                                                                      File: main.tf                                                                                                            
+resource "azurerm_resource_group" "rg" {
+  name     = "rg_np_security_tf"
+  location = "australiaeast"
 }
 
-resource ""
+resource "azurerm_public_ip" "example" {
+  name                = "public_ip_np_lb"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+
+}
+
+resource "azurerm_lb" "lb" {
+  name                = "lb_np_tf"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
+
+  frontend_ip_configuration {
+    name                 = "PubliIPAddress"
+    public_ip_address_id = azurerm_public_ip.example.id
+
+  }
+
+}
+
+
+resource "azurerm_lb_backend_address_pool" "example" {
+  loadbalancer_id = azurerm_lb.lb.id
+  name            = "BackendAddresspool"
+}
+
+
+
+
+
+
+
+
+
